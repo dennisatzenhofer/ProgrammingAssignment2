1f326b133dc20f068a5fa8559a1bd1b4e50f45fa
dennisatzenhofer <dennis-atzenhofer@live.de>
2016-07-31 23:06:10 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve can be used to cache the inverse of a matrix and return it.
 
-## Write a short comment describing this function
+## This is function should cache in the inverse of a matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function (x = as.matrix())  {
+    m <<- x 
+    inv <<- solve(m)
+    list(matrix = m, inverse = inv) # creating an object containing the original matrix and the inverse
+  }
+  
+## cacheSolve makes use of the inverse that was cached in by the function makeCacheMatrix. It returns the cached inverse or calculates the inverse
+## if the object does not contain cached data.
 
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+## By checking whether x is atomic, one can control whether the object is a regular matrix. If so, one first needs to run the makeCacheMatrix, in order to
+## get an object that can be used for the cacheSolve function. 
+  if (is.atomic(x)) {
+    message("first run function 'makeCacheMatrix'")
+  }
+  else {
+  m <- x$inverse
+  if(!is.null(m)) {                     ## using cached inverse
+    message("getting cached data")
+    return(m)
+  }
+  raw_matrix <- x$matrix
+  inv <- solve(raw_matrix)              ## calculating inverse if no cached data available
+  x$inverse <- inv
+  inv
+  }
 }
